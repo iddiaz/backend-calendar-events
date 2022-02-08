7b0edc8e7f03c10e5bf909334381095feb27e14e
iddiaz <correo@ivandiazdiaz.com>
2022-02-08 12:51:24 +0100

    Fuerza pasar por index

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+//Redirección para cuando la ruta no se conoce
+app.get('*', (req, res)=>{
+   res.sendFile(path.resolve(__dirname, "../public", "index.html"));
+});
+
 
 //escuchar peticiones
 app.listen( process.env.PORT , ()=>{
